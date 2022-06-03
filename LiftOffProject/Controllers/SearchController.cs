73613ea33529c089d92689e2e0d5cc5fab976e31
Shavonne Hylton <shavonne.hylton@gmail.com>
2022-06-03 13:31:17 -0400

    Added Search controller

--- LiftOffProject/Controllers/SearchController.cs
+++ LiftOffProject/Controllers/SearchController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LiftOffProject.Models;
+using LiftOffProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 
 namespace LiftOffProject.Controllers
 {
-    public class Wine : Controller
+    public class SearchController : Controller
     {
+        private ApplicationDbContext context;
+
+        public SearchController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+
         public IActionResult Index()
         {
             return View();
---
+++ LiftOffProject/Controllers/WineController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiftOffProject.Models;
+
+
+namespace LiftOffProject.Controllers
+{
+    public class WineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
